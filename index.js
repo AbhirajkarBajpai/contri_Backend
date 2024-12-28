0999fd39545450af1c9a6d2d575d6a72a65f6ef0
Abhirajkar Bajpai <21IT3001@rgipt.ac.in>
2024-12-29 00:24:55 +0530

    cookie problem fixing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express(); // Initialize the app first
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://contri-frontend.vercel.app" }));
 
 // Middleware
 app.use(cookieParser());
