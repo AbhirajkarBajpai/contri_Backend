72d06d5fbce571d485bc1a3a1b8f6ecd2acc9f6a
AbhirajkarBajpai <21IT3001@rgipt.ac.in>
2026-02-11 15:43:04 +0530

    secutity-fix:safari

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,8 +19,9 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     sameSite: "None", 
+    secure: true,
   };
   // if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
@@ -119,8 +120,9 @@ exports.logout = (req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    // secure: process.env.NODE_ENV === "production",
     sameSite: "None",
+    secure: true,
   };
   const token = "logout";
 
